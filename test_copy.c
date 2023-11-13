b0f2bf7f149d52e8132bfca389f1c815067f7f44
Viktor Sinagl <vsinagl@pop-os.localdomain>
2023-11-14 00:40:57 +0100

    added vs code debug config to debug executable by the gdb. Get_next_line working for different buffer sizes, but memory allocation errors in Valgrind. Memory leaks fine (just one leak from static variable but this cannot be avoid i think

--- test_copy.c
+++ test_copy.c
@@ -4,8 +4,9 @@
 #include  <fcntl.h>
 #include <stdio.h>
 #include "get_next_line.h"
+
 //global variables
-int BUFFER_SIZE = 5;
+int BUFFER_SIZE = 2000;
 char *BUFF_TST[20];
 
 size_t	ft_strlen(const char *str)
@@ -27,17 +28,17 @@ size_t	ft_strchr_m(const char *s, int c)
 
 	res = (char *)s;
 	if (c > 127)
-		return (res);
+		return (0);
 	i = 0;
 	while (res[i] != '\0')
 	{
 		if (res[i] == c)
-			return (res);
+			return (i);
 		i++;
 	}
 	if (res[i] == c)
 		return (i);
-	return (NULL);
+	return (0);
 }
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
@@ -107,68 +108,33 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 
 
-char	*newline_join(char *buff, char *buff_read, char ***cursor)
+char	*newline_join(char *buff, char *buff_read, char **cursor)
 {
 		size_t		n;
 		size_t		i;
-		char		*tmp;
+		char			*left;
+		char			*tmp;
 
-		n = ft_strchar_m(buff_read);
-		buff = (char *)malloc((n + 1) * sizeof(char));
-		while (buff_read != '\n')
-			buff[i++] = buff_read++;
-		buff[i] = '\n';
-		buff[i++] = '0';	
-		if (**cursor != NULL)
-			free(cursor);
-		**cursor = (char *)malloc((ft_strlen(buff_read) - n + 1) * sizeof(char))
+		tmp = *cursor;
+		n = ft_strchr_m(buff_read, '\n');
+		left = (char *)malloc((n + 1) * sizeof(char));
 		i = 0;
-		while (++buff_read != '\0')
-			**cursor[i++] = buff_read;
-		**cursor[i] = '0';
+		while (*buff_read != '\n')
+		{
+			left[i++] = *buff_read;
+			buff_read++;
+		}
+		left[i] = '\n';
+		left[++i] = '\0';	
+		if (*buff == '\0')
+			buff = left;	
+		else
+			buff = ft_strjoin(buff,left);
+		*cursor = ft_strdup(++buff_read);
+		if (tmp != NULL)
+			free(tmp);
 		return (buff);
 }
-
-char	*newline_join(char *buff, char *buff_read, char ***cursor)
-{
-	size_t		i;
-	size_t 		j;
-	char		*str_left;
-	char		*tmp;	
-
-
-	i = 0;
-	while (buff_read[i] != '\n')
-		i++;
-	str_left = (char *)malloc(sizeof(char) * (i + 2));
-	// printf("the strlen: %zu\n",ft_strlen(buff_read));
-	// printf("the i: %zu\n",i);
-	// printf("the malloc size of cursor: %zu\n", ft_strlen(buff_read) - 1 - i);
-	//cursor = (char **)malloc(sizeof(char) * (ft_strlen(buff_read) - i -1));
-	i = 0;
-	while(buff_read[i] != '\n')
-	{
-		str_left[i] = buff_read[i];
-		i++;
-	}
-	str_left[i] = '\n';
-	str_left[i++] = '\0';
-	tmp = buff;
-	buff = ft_strjoin(buff, str_left);
-	free(tmp);
-	j = 0;
-	**cursor = ft_strdup("ja");
-	// while(buff_read[i] != '\0')
-	// {
-	// 	cursor[j] = buff_read[i];
-	// 	j++; 
-	// 	i++;
-	// }
-	//*cursor[j] = '\0';
-	return (buff);
-}
-
-//NEW_LINE JOIN FUNCTION HERE
 
 char	*read_newline(int fd, char *buff, char **cursor, char *buff_read)
 {
@@ -185,13 +151,13 @@ char	*read_newline(int fd, char *buff, char **cursor, char *buff_read)
 		if(ft_strchr_m(buff_read, '\n') || read_err < BUFFER_SIZE)
 		{
 			if (ft_strchr_m(buff_read, '\n'))
-				buff = newline_join(buff, buff_read, &cursor);
+				buff = newline_join(buff, buff_read, cursor);
 			else if (read_err < BUFFER_SIZE)
 				buff = ft_strjoin(buff, buff_read);
 			free(tmp);
 			return(buff);
 		}
-		buff = ft_strjoin(buff, buff_read);	
+		buff = ft_strjoin(buff, buff_read);
 		free(tmp);
 	}
 }
@@ -203,18 +169,21 @@ char *get_next_line(int fd)
 	static char	*cursor;
 	char		*buff;
 	char		*buff_read;
-	ssize_t		err;
 
-	if (fd < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (cursor == NULL)
 		buff = ft_strdup("");
 	else
 		buff = cursor;
 	buff_read = (char *)malloc(sizeof(char)*(BUFFER_SIZE + 1));
+	memset(buff_read, '\0', BUFFER_SIZE + 1);
 	if (buff_read == NULL)
 		return(NULL);
-	buff = read_newline(fd, buff, &cursor, buff_read);
+	if (ft_strchr_m(buff, '\n'))
+		buff = newline_join("",buff, &cursor);
+	else
+		buff = read_newline(fd, buff, &cursor, buff_read);
 	free(buff_read);
 	if (buff == NULL)
 		return (NULL);
@@ -225,44 +194,14 @@ int main(void)
 {
 	int fd = open("readtext",O_RDONLY);
 	char *a = get_next_line(fd);
-	printf("%s\n",a)	;	
+	printf("%s",a)	;	
 	free(a);
-	char *b = get_next_line(fd);
-	printf("%s\n",b)	;	
-	free(b);
+	// char *b = get_next_line(fd);
+	// printf("%s",b)	;	
+	// b = get_next_line(fd);
+	// printf("%s",b)	;	
+	// free(b);
+	free(cursor);
+	close(fd);
 	return (0);
 }
-
-//main pro cvicny test memory leak, pri prehravvani hodnoty buffer (tam kam se ukladaji buffer_read stringy, je treba prubezne uvolnovat pres pomocny pointer, jinak nam v pameti zustava misto)
-// int		main(void)
-// {
-// 	char *tst;
-// 	char *tst2;
-// 	char *tst3;
-// 	char *tmp;
-
-// 	//testing freeing null pointer
-// 	tst = NULL;
-// 	tmp = tst;
-// 	if (tmp != NULL)
-// 		printf("memory allocated\n");
-
-// 	//first init
-// 	tst = ft_strdup("ahoj");
-
-
-// 	//loop 1
-// 	tmp = tst;
-// 	tst2 = ft_strdup(" jak se mas\n");
-// 	tst = ft_strjoin(tst,tst2);	
-// 	free(tmp);
-
-// 	//loop 2
-// 	tmp = tst;
-// 	tst3 = ft_strdup("--jde to\n");
-// 	tst = ft_strjoin(tst, tst3);
-// 	free(tmp);
-
-// 	printf("%s",tst);
-// 	free(tst), free(tst2), free(tst3); 
-// }
