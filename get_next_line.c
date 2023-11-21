2c7e394501c86c33a0c6672332cb350ff2256d2a
Viktor Sinagl <vsinagl@pop-os.localdomain>
2023-11-21 21:02:02 +0100

    the read-err = 0  error (reading the last line)

--- get_next_line.c
+++ get_next_line.c
@@ -22,11 +22,11 @@ char	*newline_join(char *buff, char *buff_read, char **cursor)
 		size_t		n;
 		size_t		i;
 		char			*left;
-		char			*tmp;
-		char			*buff_r_origin;
+		//char			*tmp;
+		//char			*buff_r_origin;
 
-		buff_r_origin = buff_read;
-		tmp = *cursor;
+		//buff_r_origin = buff_read;
+		//tmp = *cursor;
 		n = ft_strchr_m(buff_read, '\n');
 		left = (char *)malloc((n + 1) * sizeof(char));
 		i = 0;
@@ -39,8 +39,8 @@ char	*newline_join(char *buff, char *buff_read, char **cursor)
 		else
 			buff = ft_strjoin(buff,left);
 		*cursor = ft_strdup(++buff_read);
-		if (tmp != NULL && tmp != buff_r_origin)
-		 	free(tmp);
+		// if (tmp != NULL && tmp != buff_r_origin)
+		//  	free(tmp);
 		return (buff);
 }
 
@@ -49,27 +49,31 @@ char	*read_newline(int fd, char *buff, char **cursor, char *buff_read)
 	ssize_t		read_err;
 	char		*tmp;
 
-	while(1)
+	read_err = 1;
+	while(read_err > 0)
 	{
 		tmp = buff;
 		read_err = read(fd, buff_read, BUFFER_SIZE);
 		buff_read[read_err] = '\0';
 		if (read_err < 0)
-			return (NULL);
+			return(NULL);
+		if (read_err == 0)
+			break;
 		if (ft_strchr_m(buff_read, '\n'))
 		{
 			buff = newline_join(buff, buff_read, cursor);
 			return (buff);
 		}
-		if (read_err < BUFFER_SIZE)
-		{
-			buff = ft_strjoin(buff, buff_read);
-			free(tmp);
-			return(buff);
-		}
+		// if (read_err < BUFFER_SIZE)
+		// {
+		// 	buff = ft_strjoin(buff, buff_read);
+		// 	free(tmp);
+		// 	return(buff);
+		// }
 		buff = ft_strjoin(buff, buff_read);
 		free(tmp);
 	}
+	return(buff);
 }
 
 
@@ -85,15 +89,19 @@ char *get_next_line(int fd)
 	if (cursor == NULL)
 		buff = ft_strdup("");
 	else
-		buff = cursor;
+	{
+		buff = ft_strdup(cursor);
+		free(cursor);
+	}
 	buff_read = (char *)malloc(sizeof(char)*(BUFFER_SIZE + 1));
-	ft_memset(buff_read, '\0', BUFFER_SIZE + 1);
+	//ft_memset(buff_read, '\0', BUFFER_SIZE + 1);
 	if (buff_read == NULL)
 		return(NULL);
 	if (ft_strchr_m(buff, '\n'))
 		buff = newline_join("",buff, &cursor);
 	else
 		buff = read_newline(fd, buff, &cursor, buff_read);
+//	if (*buff_read != *cursor)
 	free(buff_read);
 	if (buff == NULL)
 		return (NULL);
